254202b4928167533c812fca28b70fa6b8100f80
Phillip Cook <philcsound@gmail.com>
2025-01-15 23:00:01 -0800

    Adjusted to new subsystem constructor, removed includes from cpp file to header

--- src/GroundReactions/GroundReactionsSubsystem.hpp
+++ src/GroundReactions/GroundReactionsSubsystem.hpp
@@ -1,36 +1,39 @@
 #pragma once
-
 #include "inc/Subsystem.hpp"
 #include "gtkmm.h"
 #include <vector>
 #include "Contact.hpp"
+#include <iostream>
+#include <assert.h>
+#include "inc/XML_api.hpp"
 
-class GroundReactionsSubsystem : public Subsystem {
+class GroundReactionsSubsystem : public Subsystem
+{
 public:
-    GroundReactionsSubsystem();
+    GroundReactionsSubsystem(std::shared_ptr<Gtk::Application> &app);
     void Create();
 
 private:
     std::vector<Contact> contacts;
 
-    class LandingGearSetupDialog : public Gtk::Dialog {
-    public: 
+    class LandingGearSetupDialog : public Gtk::Dialog
+    {
+    public:
         LandingGearSetupDialog(
-            const std::string& contactName, 
-            const std::string& contactType, 
-            const std::tuple<std::string, std::string, std::string>& locationCoordinates, 
-            const std::string& locationUnit, 
-            const std::string& springCoefficient, 
-            const std::string& springCoefficientUnit, 
-            const std::string& dampingCoefficient, 
-            const std::string& dampingCoefficientUnit, 
-            const std::string& staticFriction, 
-            const std::string& dynamicFriction, 
-            const std::string& rollingFriction, 
-            const std::string& maxSteer, 
-            const std::string& maxSteerUnit, 
-            const std::string& brakeGroup, 
-            const std::string& retractable
-        );
+            const std::string &contactName,
+            const std::string &contactType,
+            const std::tuple<std::string, std::string, std::string> &locationCoordinates,
+            const std::string &locationUnit,
+            const std::string &springCoefficient,
+            const std::string &springCoefficientUnit,
+            const std::string &dampingCoefficient,
+            const std::string &dampingCoefficientUnit,
+            const std::string &staticFriction,
+            const std::string &dynamicFriction,
+            const std::string &rollingFriction,
+            const std::string &maxSteer,
+            const std::string &maxSteerUnit,
+            const std::string &brakeGroup,
+            const std::string &retractable);
     };
 };
